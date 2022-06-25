738e68e2534ac8573d7b460ab8dbba6e5930ef57
egor-rusnak <yehor.rusnak9982@gmail.com>
2022-06-25 15:56:50 +0300

    Added projects, gitignore, backend editorconfig

---
+++ backend/WorkScheduler.API/Extensions/ServicesExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace WorkScheduler.API.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
